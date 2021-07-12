e89e6704ca586cb38df399fd9b6cf675371ac2b9
Fredy Somy <fredysomy@gmail.com>
2021-07-12 14:39:09 +0530

    changed all default stuffs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ module.exports = {
         },
         
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/ProxPi/proxpi',
           label: 'GitHub',
           position: 'right',
         },
@@ -89,13 +89,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/ProxPi/proxpi/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/ProxPi/proxpi',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
